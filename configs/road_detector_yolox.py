7f8fd3ad3ce5fab9eb100d93d43ea1e9b416309e
Jørgen Rosager <joergen.rosager@gmail.com>
2022-11-14 16:29:40 +0100

    small fix on path

--- configs/road_detector_yolox.py
+++ configs/road_detector_yolox.py
@@ -112,13 +112,13 @@ data = dict(
     train=train_dataset,
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file="/cluster/home/jorgro/train.txt",
+        img_prefix=data_root + 'train/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file="/cluster/home/jorgro/train.txt",
+        img_prefix=data_root + 'train/',
         pipeline=test_pipeline))
 
 # optimizer
