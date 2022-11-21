5805b5d7f733d492b5faee24faec8eb7d28d55e2
Jørgen Rosager <joergen.rosager@gmail.com>
2022-11-21 09:44:52 +0100

    fix test config

--- configs/road_detector_yolo.py
+++ configs/road_detector_yolo.py
@@ -52,10 +52,10 @@ model = dict(
     test_cfg=dict(
         nms_pre=1000,
         min_bbox_size=0,
-        score_thr=0.05,
+        score_thr=0.2,
         conf_thr=0.005,
         nms=dict(type='nms', iou_threshold=0.45),
-        max_per_img=100))
+        max_per_img=2))
 # dataset settings
 dataset_type = 'RDD2022Dataset'
 data_root = '/cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway/'
