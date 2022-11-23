17c6ee0c78acdc66a914b7a65a6d043affd1f226
Jørgen Rosager <joergen.rosager@gmail.com>
2022-11-23 11:12:03 +0100

    try make inference faster

--- tools/inference.py
+++ tools/inference.py
@@ -10,18 +10,18 @@ checkpoint_file = 'runs/latest.pth'
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
 # test a single image and show the results
-test_dataset = glob.glob("/cluster/home/jorgro/datasets/Norway/test/images/*.jpg")
+test_dataset = sorted(glob.glob("/cluster/home/jorgro/datasets/Norway/test/images/*.jpg"))
 save_file = "/cluster/home/jorgro/submission.txt"
 
-
 with open(save_file, 'w') as f:
-    for img in test_dataset:
-        result = inference_detector(model, img)
-        string = f"{img[49:]}"
-        for i, obj in enumerate(result):
-            if obj.shape[0]:
-                string += f" {i+1} {int(obj[0][0])} {int(obj[0][1])} {int(obj[0][2])} {int(obj[0][3])}"
-        string += "\n"
-        f.write(string)
-        print(string)
+    for j in range(len(test_dataset), 8):
+        results = inference_detector(model, test_dataset[j:j+8])
+        for ind, result in enumerate(results):
+            string = f"{test_dataset[j+ind][49:]}"
+            for i, obj in enumerate(result):
+                if obj.shape[0]:
+                    string += f" {i+1} {int(obj[0][0])} {int(obj[0][1])} {int(obj[0][2])} {int(obj[0][3])}"
+            string += "\n"
+            f.write(string)
+            print(string)
        # f.write(f"{}")
