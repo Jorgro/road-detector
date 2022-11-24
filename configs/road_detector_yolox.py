350dd476a7ebad4853c00d511a0ad0cac7827ed1
Jørgen Rosager <joergen.rosager@gmail.com>
2022-11-24 14:12:24 +0100

    make yolox small

--- configs/road_detector_yolox.py
+++ configs/road_detector_yolox.py
@@ -9,14 +9,14 @@ model = dict(
     input_size=img_scale,
     random_size_range=(15, 25),
     random_size_interval=10,
-    backbone=dict(type='CSPDarknet', deepen_factor=1.33, widen_factor=1.25),
+    backbone=dict(type='CSPDarknet', deepen_factor=0.33, widen_factor=0.5),
     neck=dict(
         type='YOLOXPAFPN',
-        in_channels=[320, 640, 1280],
-        out_channels=320,
+        in_channels=[128, 256, 512],
+        out_channels=128,
         num_csp_blocks=4),
     bbox_head=dict(
-        type='YOLOXHead', num_classes=4, in_channels=320, feat_channels=320),
+        type='YOLOXHead', num_classes=4, in_channels=128, feat_channels=128),
     train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5)),
     # In order to align the source code, the threshold of the val phase is
     # 0.01, and the threshold of the test phase is 0.001.
